dcbcf1a5f62362ef7793d3831c15150dea76fc5d
David Lahm <david@klinger-lahm.de>
2019-03-15 09:04:31 +0100

    Comments updated

--- src/MaybeMonad.php
+++ src/MaybeMonad.php
@@ -3,19 +3,18 @@
 namespace Hllizi\PHPMonads;
 
 /**
- * MaybeMonad 
- * 
+ * MaybeMonad
+ *
  * @package phpmonads
  * @version $id$
  * @copyright 2019 David Lahm
- * @author David Lahm 
+ * @author David Lahm
  * @license GPL-2
  *
- * Implementation of the Maybe monad. Implements the MonadInterface uses the MonadTrait, implementing its abstract methods. Additionally, the method getValue() is provided in order to extract computation results.
+ * Implementation of the Maybe monad. Implements the MonadInterface uses the MonadTrait, implementing its abstract methods. Additionally, the method getValue() is provided in order to extract computation results and the method toArrayObject() to turn objects into corresponding ArrayObjects. The prototype of the ArrayObject can be set with setArrayObjectPrototype().
  */
-
 class MaybeMonad
-	implements MonadInterface
+    implements MonadInterface
 {
     use MonadTrait;
 
@@ -36,9 +35,9 @@ class MaybeMonad
      */
     public function bind(callable $method)
     {
-        if($this->isSomething()) {
-		    $newMaybeMonad = call_user_func($method, $this->value);
-		    return $newMaybeMonad->setArrayObjectPrototype($this->arrayObjectPrototype);
+        if ($this->isSomething()) {
+            $newMaybeMonad = call_user_func($method, $this->value);
+            return $newMaybeMonad->setArrayObjectPrototype($this->arrayObjectPrototype);
         } else {
             return $this;
         }
@@ -54,7 +53,7 @@ class MaybeMonad
      */
     public function return($x)
     {
-        if(isset($x)) {
+        if (isset($x)) {
             return (new MaybeMonad($x))->setArrayObjectPrototype($this->arrayObjectPrototype);
         } else {
             throw new IllicitValueException("MaybeMonad::return() cannot be used with null argument (use constructor instead).");
@@ -100,20 +99,20 @@ class MaybeMonad
      * @access public
      * @return ArrayMonad
      *
-     * Turn the object into a singleton ArrayMonad. (Dependencies here should be inverted).
+     * Turn the object into a singleton ArrayObject, according to the prototype provided. With no prototype given, an ArrayMonad will be created.
      */
 
     public function toArrayObject()
     {
-	$arrayObject = isset($this->arrayObjectPrototype) ? (clone $this->arrayObjectPrototype) : new ArrayMonad();
-	$arrayObject->exchangeArray($this->isSomething() ? [$this->value] : []);
-	return $arrayObject;
+        $arrayObject = isset($this->arrayObjectPrototype) ? (clone $this->arrayObjectPrototype) : new ArrayMonad();
+        $arrayObject->exchangeArray($this->isSomething() ? [$this->value] : []);
+        return $arrayObject;
     }
 
     public function setArrayObjectPrototype($arrayObjectPrototype)
     {
-	    $this->arrayObjectPrototype = $arrayObjectPrototype;
-	    return $this;
+        $this->arrayObjectPrototype = $arrayObjectPrototype;
+        return $this;
     }
 }
 /**
