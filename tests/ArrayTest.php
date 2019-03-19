97a7b3d555ad20d3724e655b3602623de33e18fe
David Lahm <david@klinger-lahm.de>
2019-03-19 16:07:06 +0100

    Test for fold.

--- tests/ArrayTest.php
+++ tests/ArrayTest.php
@@ -9,7 +9,14 @@
 namespace Hllizi\PHPMonads;
 
 
-class ArrayTest
-{
+use PHPUnit\Framework\TestCase;
 
+class ArrayTest extends TestCase
+{
+    public function testFold()
+    {
+        $testObject = new ArrayMonad([1,2,3]);
+        $this->assertEquals(6, $testObject->fold(0, function ($i, $j) {return $i+$j;}));
+        $this->assertEquals(6, $testObject->fold(1, function ($i, $j) {return $i*$j;}));
+    }
 }
