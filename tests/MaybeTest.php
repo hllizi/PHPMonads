c61b6d536fa45d96e9167d57731625b88fec7571
David Lahm <david@klinger-lahm.de>
2020-01-07 17:08:14 +0100

    issue-3: added test cases for getWithAlternative (and in passing also for isNothing() and isSomething()

--- tests/MaybeTest.php
+++ tests/MaybeTest.php
@@ -4,7 +4,30 @@
 namespace Hllizi\PHPMonads;
 
 
-class MaybeTest
-{
+use PHPUnit\Framework\TestCase;
 
+class MaybeTest extends TestCase
+{
+    private $nothing;
+    private $something;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        $this->nothing = new MaybeMonad(null);
+        $this->something = new MaybeMonad("Hi");
+        parent::__construct($name, $data, $dataName);
+    }
+
+    public function testSomethingNothing() {
+        $this->assertTrue($this->nothing->isNothing());
+        $this->assertTrue($this->something->isSomething());
+        $this->assertFalse($this->nothing->isSomething());
+        $this->assertFalse($this->something->isNothing());
+    }
+
+    public function testGetWithAlternative()
+    {
+        $this->assertEquals("Test", $this->nothing->getWithAlternative("Test"));
+        $this->assertEquals("Test", $this->something->getWithAlternative("Test"));
+    }
 }
